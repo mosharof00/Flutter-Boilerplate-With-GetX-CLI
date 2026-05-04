9df757f8a30f83fdffd7a682dc464adddee9041d
Mosharof Khan <mosharof5khan@gmail.com>
2026-05-04 12:23:39 +0600

    Update

--- lib/app/modules/products/controllers/products_controller.dart
+++ lib/app/modules/products/controllers/products_controller.dart
@@ -6,29 +6,106 @@ import 'package:get/get.dart';
 import '../../../core/network/handle_exceptions.dart';
 import '../../../data/repositories/product_repository.dart';
 
-class ProductsController extends GetxController with PaginatedFetchMixin {
+///   Using PaginatedFetchMixin
+// class ProductsController extends GetxController with PaginatedFetchMixin {
+//   final IProductRepository _repo = Get.find<IProductRepository>();
+//
+//   final productList = <Product>[].obs;
+//
+//   @override
+//   Future<void> fetchPage(int page) async {
+//     final response = await _repo.getProducts(
+//       limit: pagination,
+//       skip: page * pagination,
+//     );
+//
+//     final products = response.products ?? [];
+//
+//     if (page == pageBase) {
+//       productList.value = products;
+//     } else {
+//       productList.addAll(products);
+//     }
+//
+//     if (products.length < pagination) isEndPage = true;
+//   }
+//
+//   @override
+//   void onFetchError(dynamic e) => handleException(e);
+// }
+
+class ProductsController extends GetxController {
   final IProductRepository _repo = Get.find<IProductRepository>();
 
   final productList = <Product>[].obs;
 
-  @override
-  Future<void> fetchPage(int page) async {
-    final response = await _repo.getProducts(
-      limit: pagination,
-      skip: page * pagination,
-    );
+  final isFetching = false.obs;
+  final isLoadingMore = false.obs;
+  bool isEndPage = false;
+  bool _isInitialFetch = true;
+  final int pagination = 10;
+  int currentPage = 0;
+  final scrollController = ScrollController();
 
-    final products = response.products ?? [];
+  Future<void> fetchData({bool isRefresh = true}) async {
+    if (isLoadingMore.value) return;
 
-    if (page == pageBase) {
-      productList.value = products;
+    if (isRefresh) {
+      currentPage = 0;
+      isEndPage = false;
+      if (_isInitialFetch) {
+        isFetching.value = true;
+      }
     } else {
-      productList.addAll(products);
+      isLoadingMore.value = true;
     }
 
-    if (products.length < pagination) isEndPage = true;
+    try {
+      final response = await _repo.getProducts(
+        limit: pagination,
+        skip: currentPage * pagination,
+      );
+
+      final products = response.products ?? [];
+
+      if (currentPage == 0) {
+        productList.value = products;
+      } else {
+        productList.addAll(products);
+      }
+
+      if (products.length < pagination) isEndPage = true;
+      currentPage++;
+    } catch (e) {
+      handleException(e);
+    } finally {
+      isFetching.value = false;
+      isLoadingMore.value = false;
+      _isInitialFetch = false;
+    }
+  }
+
+  void onScrollListener() {
+    if (scrollController.position.pixels >=
+            scrollController.position.maxScrollExtent - 200 &&
+        !isEndPage &&
+        !isLoadingMore.value &&
+        !isFetching.value) {
+      fetchData(isRefresh: false);
+    }
   }
 
   @override
-  void onFetchError(dynamic e) => handleException(e);
+  void onInit() {
+    scrollController.addListener(onScrollListener);
+    fetchData();
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    scrollController.removeListener(onScrollListener);
+    scrollController.dispose();
+    super.onClose();
+  }
 }
